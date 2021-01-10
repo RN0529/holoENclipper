bf1aed73b5b06f75e4cd66779a6f3a7b42cf42bc
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2021-01-10 16:12:16 -0500

    a little backend search added

---
+++ DopimineSearchPrototype/search.js
@@ -0,0 +1,24 @@
+function getVideo() {
+    $.ajax({
+        type: 'GET',
+        url: 'https://www.googleapis.com/youtube/v3/search',
+        data: {
+            key: 'AIzaSyDNHAYWSCF8gGh0g-6lqahCljqt_fZRy-c',
+            q: "gura clip",
+            part: 'snippet',
+            maxResults: 1,
+            type: 'video',
+            videoEmbeddable: true,
+        },
+        success: function(data){
+            embedVideo(data)
+        },
+        error: function(response){
+            console.log("Request Failed");
+        }
+    });
+}
+
+function embedVideo(data) {
+    $('iframe').attr('src', 'https://www.youtube.com/embed/' + data.items[0].id.videoId)
+}
