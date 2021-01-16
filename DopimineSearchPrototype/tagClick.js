e50a101ae40e3d05bd5fc8107d157944c206574c
RN0529 <66885775+RN0529@users.noreply.github.com>
2021-01-16 15:30:23 -0500

    finished the onclicks for all the buttons and implemented the reset feature of the random button

--- DopimineSearchPrototype/tagClick.js
+++ DopimineSearchPrototype/tagClick.js
@@ -23,6 +23,42 @@ function randOnClick(btn){
   else {
     btn.style.backgroundColor  = 'gray';
     (btn).style.borderStyle = 'inset';
-
+    //uncheck other buttons when random is clicked
+    unClickOthers();
   }
+}
+
+function unClickOthers(){
+  var btn1 = document.getElementById('ddBtn');
+  btn1.style.background = 'white';
+  (btn1).style.borderStyle = 'outset';
+
+  var btn2= document.getElementById('painBtn');
+  btn2.style.background = 'white';
+  (btn2).style.borderStyle = 'outset';
+
+  var btn3= document.getElementById('wholeBtn');
+  btn3.style.background = 'white';
+  (btn3).style.borderStyle = 'outset';
+
+  var btn4= document.getElementById('mknBtn');
+  btn4.style.background = 'white';
+  (btn4).style.borderStyle = 'outset';
+
+  var btn5= document.getElementById('deskBtn');
+  btn5.style.background = 'white';
+  (btn5).style.borderStyle = 'outset';
+
+  var btn6= document.getElementById('sneezeBtn');
+  btn6.style.background = 'white';
+  (btn6).style.borderStyle = 'outset';
+
+  var btn7= document.getElementById('BLBtn');
+  btn7.style.background = 'white';
+  (btn7).style.borderStyle = 'outset';
+
+  var btn8= document.getElementById('ABtn');
+  btn8.style.background = 'white';
+  (btn8).style.borderStyle = 'outset';
+
 }
