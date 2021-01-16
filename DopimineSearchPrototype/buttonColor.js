8101344e5858819d82963826bc804d4a0d7db785
RN0529 <66885775+RN0529@users.noreply.github.com>
2021-01-16 15:35:05 -0500

    Chenged all querySelectors to be getelementsbyid instead

--- DopimineSearchPrototype/buttonColor.js
+++ DopimineSearchPrototype/buttonColor.js
@@ -1,6 +1,6 @@
 
 // select the button
-var  btnGura = document.querySelector('#guraButton');
+var  btnGura = document.getElementById('guraButton');
 // add a click event listener
 btnGura.addEventListener('click', function () {
   if (this.style.borderStyle === 'inset') {
@@ -11,13 +11,13 @@ btnGura.addEventListener('click', function () {
     this.style.background = '#343B86';
     (this).style.borderStyle = 'inset';
     //change random button to off
-    var btnRnd2 = document.querySelector('#randButton');
+    var btnRnd2 = document.getElementById('randButton');
     btnRnd2.style.background = 'gray';
     (btnRnd2).style.borderStyle = 'outset';
   }
 });
 
-var btnIna = document.querySelector('#inaButton');
+var btnIna = document.getElementById('inaButton');
 btnIna.addEventListener('click', function () {
     if (this.style.borderStyle === 'inset') {
       this.style.background = '#9510AC';
@@ -27,13 +27,13 @@ btnIna.addEventListener('click', function () {
       this.style.background = '#590A68';
       (this).style.borderStyle = 'inset';
       //change random button to off
-      var btnRnd2 = document.querySelector('#randButton');
+      var btnRnd2 = document.getElementById('randButton');
       btnRnd2.style.background = 'gray';
       (btnRnd2).style.borderStyle = 'outset';
     }
   });
 
-var btnAme = document.querySelector('#ameButton');
+var btnAme = document.getElementById('ameButton');
 btnAme.addEventListener('click', function () {
     if (this.style.borderStyle === 'inset') {
       this.style.background = '#EAB15F';
@@ -43,14 +43,14 @@ btnAme.addEventListener('click', function () {
       this.style.background = '#B9854B';
       (this).style.borderStyle = 'inset';
       //change random button to off
-      var btnRnd2 = document.querySelector('#randButton');
+      var btnRnd2 = document.getElementById('randButton');
       btnRnd2.style.background = 'gray';
       (btnRnd2).style.borderStyle = 'outset';
     }
 });
 
 
-var btnKiara = document.querySelector('#kiaraButton');
+var btnKiara = document.getElementById('kiaraButton');
 btnKiara.addEventListener('click', function () {
     if (this.style.borderStyle === 'inset') {
       this.style.background = '#FF9300';
@@ -60,14 +60,14 @@ btnKiara.addEventListener('click', function () {
       this.style.background = '#AC6606';
       (this).style.borderStyle = 'inset';
       //change random button to off
-      var btnRnd2 = document.querySelector('#randButton');
+      var btnRnd2 = document.getElementById('randButton');
       btnRnd2.style.background = 'gray';
       (btnRnd2).style.borderStyle = 'outset';
     }
   });
 
 
-var btnCali = document.querySelector('#moriButton');
+var btnCali = document.getElementById('moriButton');
 btnCali.addEventListener('click', function () {
     if (this.style.borderStyle === 'inset') {
       this.style.background = '#000000';
@@ -77,13 +77,13 @@ btnCali.addEventListener('click', function () {
       this.style.background = '#2F1122';
       (this).style.borderStyle = 'inset';
        //change random button to off
-       var btnRnd2 = document.querySelector('#randButton');
+       var btnRnd2 = document.getElementById('randButton');
        btnRnd2.style.background = 'gray';
        (btnRnd2).style.borderStyle = 'outset';
     }
 });
 
-var btnRnd = document.querySelector('#randButton');
+var btnRnd = document.getElementById('randButton');
 btnRnd.addEventListener('click', function () {
     if (this.style.borderStyle === 'inset') {
       this.style.background = 'gray';
@@ -93,33 +93,27 @@ btnRnd.addEventListener('click', function () {
       this.style.background = 'black';
       $(this).css('border-style', 'inset');
 
-      var btnCali2 = document.querySelector('#moriButton');
+      var btnCali2 = document.getElementById('moriButton');
       btnCali2.style.background = '#000000';
       btnCali2.style.borderStyle = 'outset';
 
 
-      var btnKiara2 = document.querySelector('#kiaraButton');
+      var btnKiara2 = document.getElementById('kiaraButton');
       btnKiara2.style.background = '#FF9300';
       btnKiara2.style.borderStyle = 'outset';
 
-      var btnAme2 = document.querySelector('#ameButton');
+      var btnAme2 = document.getElementById('ameButton');
       btnAme2.style.background = '#EAB15F';
       (btnAme2).style.borderStyle = 'outset';
 
 
-      var btnIna2 = document.querySelector('#inaButton');
+      var btnIna2 = document.getElementById('inaButton');
       btnIna2.style.background = '#9510AC';
       (btnIna2).style.borderStyle = 'outset';
 
-      var btnGura2 = document.querySelector('#guraButton');
+      var btnGura2 = document.getElementById('guraButton');
       btnGura2.style.background = '#2D9BF0';
      (btnGura2).style.borderStyle = 'outset';
-
-
-
-
-
-
 
     }
   });
