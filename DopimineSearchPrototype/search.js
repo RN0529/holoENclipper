7f760b4beb669437616dfa46431176b463911a85
Nelson Lieu <36254391+TheStress@users.noreply.github.com>
2021-01-18 18:49:05 -0500

    finished selecting and search capability

--- DopimineSearchPrototype/search.js
+++ DopimineSearchPrototype/search.js
@@ -1,4 +1,71 @@
-function getVideo() {
+function search() {
+    var charButtons = document.getElementsByClassName("characterButton");
+    var tagButtons = document.getElementsByClassName("tagButton");
+    
+    var searchString = "";
+    var tagSelected = false;
+    var charSelected = false;
+
+    //First checking if random was selected
+    if(charButtons[charButtons.length - 1].style.borderStyle === "inset") {
+        var randomIndex = Math.random() * (charButtons.length-2);
+        if(Math.random() > 0.5) {
+            randomIndex = Math.floor(randomIndex);
+        }
+        else {
+            randomIndex = Math.ceil(randomIndex);
+        }
+        searchString += charButtons[randomIndex].getElementsByClassName("characterText")[0].innerHTML + " ";
+        charSelected = true;
+    }
+    else {
+        //Looping through checking if characters were selected
+        for(var i = 0; i < charButtons.length - 1; i++) {
+            if(charButtons[i].style.borderStyle === "inset") {
+                searchString += charButtons[i].getElementsByClassName("characterText")[0].innerHTML + " ";
+                charSelected = true;
+            }
+        }
+    }
+
+    //First checking if random was selected
+    if(tagButtons[tagButtons.length - 1].style.borderStyle === "inset") {
+        var randomIndex = Math.random() * (tagButtons.length-2);
+        if(Math.random() > 0.5) {
+            randomIndex = Math.floor(randomIndex);
+        }
+        else {
+            randomIndex = Math.ceil(randomIndex);
+        }
+        searchString += tagButtons[randomIndex].innerHTML + " ";
+        tagSelected = true;
+    }
+    else {
+        //Looping through checking if tags were selected
+        for(var i = 0; i < tagButtons.length - 1; i++) {
+            if(tagButtons[i].style.borderStyle === "inset") {
+                searchString += tagButtons[i].innerHTML + " ";
+                tagSelected = true;
+            }
+        }
+    }
+
+    if(charSelected === false && tagSelected == false) {
+        window.alert("Please Select a Member and Tag");
+        return;
+    }
+    else if(charSelected == false) {
+        window.alert("Please Select a Member");
+        return;
+    }
+    else if(tagSelected == false) {
+        window.alert("Please Select a Tag");
+        return;
+    }
+    window.alert(searchString);
+    getVideo(searchString);
+}
+function getVideo(searchString) {
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search',
@@ -6,7 +73,7 @@ function getVideo() {
             key: 'AIzaSyDNHAYWSCF8gGh0g-6lqahCljqt_fZRy-c',
             //'AIzaSyDNHAYWSCF8gGh0g-6lqahCljqt_fZRy-c' nelsons key
 
-            q: "hololive Amelia watson pain ",
+            q: searchString,
             part: 'snippet',
             maxResults: 1,
             type: 'video',
