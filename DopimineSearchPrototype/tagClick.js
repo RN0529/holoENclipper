38f99e764318a57c0ba67697936aa4e8531fd8cf
RN0529 <66885775+RN0529@users.noreply.github.com>
2021-01-18 19:19:11 -0500

    changed how the tags click function works so that only 1 of the tags can be clicked

--- DopimineSearchPrototype/tagClick.js
+++ DopimineSearchPrototype/tagClick.js
@@ -1,5 +1,6 @@
 
 function tagClick(btn){
+  
   if (btn.style.borderStyle === 'inset') {
     btn.style.background = '#45C0F1';
     (btn).style.borderStyle = 'outset';
@@ -13,9 +14,11 @@ function tagClick(btn){
     (randTag).style.borderStyle = 'outset';
 
   }
+  unClickOthers(btn);
 }
 
 function randOnClick(btn){
+
   if (btn.style.borderStyle === 'inset') {
     btn.style.background = '#45C0F1';
     (btn).style.borderStyle = 'outset';
@@ -24,41 +27,66 @@ function randOnClick(btn){
     btn.style.backgroundColor  = '#37ABEA';
     (btn).style.borderStyle = 'inset';
     //uncheck other buttons when random is clicked
-    unClickOthers();
+    unClickOthers(btn);
   }
 }
 
-function unClickOthers(){
+function unClickOthers(btnH){
   var btn1 = document.getElementById('ddBtn');
-  btn1.style.background = '#45C0F1';
-  (btn1).style.borderStyle = 'outset';
+  if(btn1!=btnH){
+    btn1.style.background = '#45C0F1';
+    (btn1).style.borderStyle = 'outset';
+  }
 
   var btn2= document.getElementById('painBtn');
-  btn2.style.background = '#45C0F1';
-  (btn2).style.borderStyle = 'outset';
+  if(btn2!=btnH){
+    btn2.style.background = '#45C0F1';
+    (btn2).style.borderStyle = 'outset';
+  }
+  
 
   var btn3= document.getElementById('wholeBtn');
-  btn3.style.background = '#45C0F1';
-  (btn3).style.borderStyle = 'outset';
+  if(btn3!=btnH){
+    btn3.style.background = '#45C0F1';
+    (btn3).style.borderStyle = 'outset';
+  }
 
   var btn4= document.getElementById('mknBtn');
-  btn4.style.background = '#45C0F1';
-  (btn4).style.borderStyle = 'outset';
+  if(btn4!=btnH){
+    btn4.style.background = '#45C0F1';
+    (btn4).style.borderStyle = 'outset';
+  }
+
 
   var btn5= document.getElementById('deskBtn');
-  btn5.style.background = '#45C0F1';
-  (btn5).style.borderStyle = 'outset';
+  if(btn5!=btnH){
+    btn5.style.background = '#45C0F1';
+    (btn5).style.borderStyle = 'outset';
+  }
+
 
   var btn6= document.getElementById('sneezeBtn');
-  btn6.style.background = '#45C0F1';
-  (btn6).style.borderStyle = 'outset';
+  if(btn6!=btnH){
+    btn6.style.background = '#45C0F1';
+    (btn6).style.borderStyle = 'outset';
+  }
 
   var btn7= document.getElementById('BLBtn');
-  btn7.style.background = '#45C0F1';
-  (btn7).style.borderStyle = 'outset';
+  if(btn7!=btnH){
+    btn7.style.background = '#45C0F1';
+    (btn7).style.borderStyle = 'outset';
+  }
 
   var btn8= document.getElementById('ABtn');
-  btn8.style.background = '#45C0F1';
-  (btn8).style.borderStyle = 'outset';
+  if(btn8!=btnH){
+    btn8.style.background = '#45C0F1';
+    (btn8).style.borderStyle = 'outset';
+  }
+
+  var btn9= document.getElementById('#randTag');
+  if(btn9!=btnH){
+    btn9.style.background = '#45C0F1';
+    (btn9).style.borderStyle = 'outset';
+  }
 
 }
